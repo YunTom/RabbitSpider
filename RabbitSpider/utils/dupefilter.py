4e0b59ae6a95eb2a51ca2e99c7ea28640ab2b7a9
YunTom <74497948+YunTom@users.noreply.github.com>
2024-06-11 10:25:33 +0800

    Update dupefilter.py

--- RabbitSpider/utils/dupefilter.py
+++ RabbitSpider/utils/dupefilter.py
@@ -6,11 +6,24 @@ import redis
 class RFPDupeFilter(object):
     def __init__(self, repeat, host,
                  port, db, password=None):
-        self.repeat = repeat
-        self.redis = redis.StrictRedis(host=host, port=port, db=db, password=password)
-        self.redis.delete(repeat)
+        if host:
+            self.repeat = repeat
+            self.redis = redis.StrictRedis(host=host, port=port, db=db, password=password)
+            self.redis.delete(repeat)
+        else:
+            self.redis = None
+            self.repeat = set()
 
     def request_seen(self, obj):
-        fingerprint = hashlib.md5(obj).hexdigest()
-        result = self.redis.sadd(self.repeat, pickle.dumps(fingerprint))
-        return result
+        fingerprint = hashlib.sha1(obj).hexdigest()
+        data = pickle.dumps(fingerprint)
+        if self.redis:
+            result = self.redis.sadd(self.repeat, data)
+            return result
+        else:
+            if data in self.repeat:
+                return False
+            else:
+                self.repeat.add(data)
+                return True
+
