91840d78e18b10a3854f36481d7100351d75fced
YunTom <74497948+YunTom@users.noreply.github.com>
2024-06-19 13:24:02 +0800

    Add files via upload

---
+++ RabbitSpider/items/__init__.py
@@ -0,0 +1,16 @@
+from abc import ABCMeta
+
+
+class Field(dict):
+    pass
+
+
+class ItemMeta(ABCMeta):
+    def __new__(mcs, name, bases, attrs):
+        field = {}
+        for key, value in attrs.items():
+            if isinstance(value, Field):
+                field[key] = value
+        cls_instance = super().__new__(mcs, name, bases, attrs)
+        cls_instance.FIELDS = field
+        return cls_instance
