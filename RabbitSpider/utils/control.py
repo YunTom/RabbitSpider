24205269685f8a55420b8918bd4a0e12eb27b32f
YunTom <74497948+YunTom@users.noreply.github.com>
2024-06-19 13:59:52 +0800

    Update control.py

--- RabbitSpider/utils/control.py
+++ RabbitSpider/utils/control.py
@@ -1,17 +1,11 @@
-import os, sys
 import asyncio
 from collections import defaultdict
 from importlib import import_module
 from typing import Final, Dict, List, Callable
-from asyncio import Task, Future, Semaphore, WindowsSelectorEventLoopPolicy
+from asyncio import Task, Future, Semaphore
 from RabbitSpider.http.request import Request
 from RabbitSpider.http.response import Response
 from RabbitSpider.core.download import CurlDownload
-
-if sys.platform == 'win32':
-    asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())
-
-sys.path.append(os.path.abspath(os.path.join(os.path.abspath(sys.argv[0]), '../..')))
 
 
 def load_class(_path):
