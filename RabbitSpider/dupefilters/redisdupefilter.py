658c3025f8241fd1245f126d60420488a8dcde7f
YunTom <74497948+YunTom@users.noreply.github.com>
2024-06-24 11:00:13 +0800

    Update redisdupefilter.py

--- RabbitSpider/dupefilters/redisdupefilter.py
+++ RabbitSpider/dupefilters/redisdupefilter.py
@@ -12,12 +12,10 @@ from urllib.parse import urlencode
 class RedisDupeFilter(DupeFilter):
     def __init__(self, settings):
         super().__init__(settings)
-        self.sha1 = hashlib.sha1()
-        repeat = os.path.basename(sys.argv[0])
-        self.repeat = repeat
+        self.repeat = os.path.basename(sys.argv[0])
         self.redis = redis.StrictRedis(host=settings.get('REDIS_HOST'), port=settings.get('REDIS_PORT'),
                                        db=settings.get('REDIS_DB'), password=settings.get('REDIS_PASSWORD'))
-        self.redis.delete(repeat)
+        self.redis.delete(self.repeat)
 
     def request_seen(self, request):
         if isinstance(request.data, (dict, list, tuple)):
@@ -34,14 +32,15 @@ class RedisDupeFilter(DupeFilter):
         if request.json is not None:
             body = json.dumps(request.json, separators=(",", ":")).encode()
 
+        sha1 = hashlib.sha1()
         if isinstance(request.params, (dict, list, tuple)):
-            self.sha1.update(f'{request.url}?{urlencode(request.params)}'.encode('utf-8'))
+            sha1.update(f'{request.url}?{urlencode(request.params)}'.encode('utf-8'))
         else:
-            self.sha1.update(request.url.encode('utf-8'))
-        self.sha1.update(request.method.encode('utf-8'))
-        self.sha1.update(body)
-        self.sha1.update(str(request.retry).encode('utf-8'))
-        fingerprint = self.sha1.hexdigest()
+            sha1.update(request.url.encode('utf-8'))
+        sha1.update(request.method.encode('utf-8'))
+        sha1.update(body)
+        sha1.update(str(request.retry).encode('utf-8'))
+        fingerprint = sha1.hexdigest()
         data = pickle.dumps(fingerprint)
         if self.redis:
             result = self.redis.sadd(self.repeat, data)
