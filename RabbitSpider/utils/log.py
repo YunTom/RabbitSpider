929bbc2c2a2694fc1fa54f1270cbfc1827dee639
YunTom <74497948+YunTom@users.noreply.github.com>
2024-06-25 10:22:29 +0800

    Update log.py

--- RabbitSpider/utils/log.py
+++ RabbitSpider/utils/log.py
@@ -3,11 +3,14 @@ from loguru import logger
 
 class Logger(object):
     def __init__(self, settings, name):
-        logger.add("%s/rabbit_{time:YYYY-MM-DD}.log" % settings.get('LOG_FILE'), level=settings.get('LOG_LEVEL'),
-                   rotation="1 day",
-                   retention="1 week",
-                   format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {extra[scope]} | {name}:{line} - {message}")
-        self.logger = logger.bind(scope=name)
+        if settings.get('LOG_FILE'):
+            logger.add("%s/rabbit_{time:YYYY-MM-DD}.log" % settings.get('LOG_FILE'), level=settings.get('LOG_LEVEL'),
+                       rotation="1 day",
+                       retention="1 week",
+                       format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {extra[scope]} | {name}:{line} - {message}")
+            self.logger = logger.bind(scope=name)
+        else:
+            self.logger = logger
 
     def log(self, msg):
         self.logger.info(msg)
