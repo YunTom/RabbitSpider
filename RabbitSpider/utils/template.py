6751c20c8ea0401cef680258cd9411c1c84e21f4
YunTom <74497948+YunTom@users.noreply.github.com>
2024-09-02 12:53:14 +0800

    Update template.py

--- RabbitSpider/utils/template.py
+++ RabbitSpider/utils/template.py
@@ -17,26 +17,27 @@ def tmpl_file_path(_path):
                 yield i
 
 
-def template_to_file(_path, **kwargs):
-    if kwargs['project'].lower() == 'test':
-        print(f'项目名称不可以是{kwargs["project"]}')
+def template_to_file(_path, _file):
+    if _path.lower() == 'test':
+        print(f'项目名称不可以是{_path}')
         return
     try:
         shutil.copytree(settings.get('TEMPLATE_DIR'), _path)
     except FileExistsError:
-        if os.path.exists(os.path.join(_path, 'spiders', f'{kwargs["spider"].lower()}.py')):
-            print(f'{kwargs["project"]}/spiders/{kwargs["spider"]}已存在')
+        if os.path.exists(os.path.join(_path, 'spiders', f'{_file}.py')):
+            print(f'{_path}/spiders/{_file}已存在')
             return
         shutil.copy(os.path.abspath(os.path.join(settings.get('TEMPLATE_DIR'), 'spiders/basic.tmpl')),
                     os.path.join(_path, 'spiders'))
     for file in tmpl_file_path(_path):
         with open(file, 'r', encoding='utf-8') as f:
-            text = Template(f.read()).substitute(**kwargs)
+            text = Template(f.read()).substitute(project=_path, spider=_file,
+                                                 classname=_file.capitalize())
         with open(file.replace('tmpl', 'py'), 'w', encoding='utf-8') as f:
             f.write(text)
         os.remove(file)
     os.rename(os.path.join(_path, 'spiders', 'basic.py'),
-              os.path.join(_path, 'spiders', f'{kwargs["spider"].lower()}.py'))
+              os.path.join(_path, 'spiders', f'{_file}.py'))
     print(f'{_path}创建完成')
 
 
@@ -46,4 +47,4 @@ def create_project():
     parser.add_argument('project', help='参数：项目名称')
     parser.add_argument('spider', help='参数：文件名称')
     args = parser.parse_args()
-    template_to_file(f'./{args.project}', **{'project': f'{args.project}', 'spider': f'{args.spider.capitalize()}'})
+    template_to_file(f'./{args.project}', _file=f'{args.spider}')
