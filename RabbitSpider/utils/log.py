07a48561f5d9170e5a230445289cf11339b58d05
YunTom <74497948+YunTom@users.noreply.github.com>
2024-09-03 10:28:37 +0800

    Update log.py

--- RabbitSpider/utils/log.py
+++ RabbitSpider/utils/log.py
@@ -1,15 +1,21 @@
+import os, sys
 from loguru import logger
 
 
 class Logger(object):
     def __init__(self, settings, name):
-        if settings.get('LOG_FILE'):
-            logger.add("%s/rabbit_{time:YYYY-MM-DD}.log" % settings.get('LOG_FILE'),
-                       level=settings.get('LOG_LEVEL', 'ERROR'),
-                       rotation="1 day",
-                       retention="1 week",
-                       format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {extra[scope]} | {name}:{line} - {message}")
-            self.logger = logger.bind(scope=name)
+        log_path = settings.get('LOG_FILE')
+        if log_path:
+            for root, dirs, files in os.walk(os.path.abspath(os.path.join(sys.argv[0],'../..'))):
+                if log_path.split('/')[0] == os.path.basename(root):
+                    log_dir = os.path.join(root, log_path.split('/')[1])
+                    logger.add("%s/rabbit_{time:YYYY-MM-DD}.log" % log_dir,
+                               level=settings.get('LOG_LEVEL', 'ERROR'),
+                               rotation="1 day",
+                               retention="1 week",
+                               format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {extra[scope]} | {name}:{line} - {message}")
+                    self.logger = logger.bind(scope=name)
+                    break
         else:
             self.logger = logger
 
