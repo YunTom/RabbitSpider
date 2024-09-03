a42391ff441ea6b455faf18bc13bfc47361efee0
YunTom <74497948+YunTom@users.noreply.github.com>
2024-09-03 10:34:16 +0800

    Update log.py

--- RabbitSpider/utils/log.py
+++ RabbitSpider/utils/log.py
@@ -1,12 +1,13 @@
 import os, sys
 from loguru import logger
+from RabbitSpider.utils.exceptions import RabbitExpect
 
 
 class Logger(object):
     def __init__(self, settings, name):
         log_path = settings.get('LOG_FILE')
         if log_path:
-            for root, dirs, files in os.walk(os.path.abspath(os.path.join(sys.argv[0],'../..'))):
+            for root, dirs, files in os.walk(os.path.abspath(os.path.join(sys.argv[0], '../..'))):
                 if log_path.split('/')[0] == os.path.basename(root):
                     log_dir = os.path.join(root, log_path.split('/')[1])
                     logger.add("%s/rabbit_{time:YYYY-MM-DD}.log" % log_dir,
@@ -16,6 +17,8 @@ class Logger(object):
                                format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {extra[scope]} | {name}:{line} - {message}")
                     self.logger = logger.bind(scope=name)
                     break
+            else:
+                raise RabbitExpect('日志目录应在当前项目下')
         else:
             self.logger = logger
 
