cf4fccf23ebb045526542ddb580adf200b5e8bda
yuntom <2395396520@qq.com>
2024-09-28 09:54:07 +0800

    1

--- RabbitSpider/__init__.py
+++ RabbitSpider/__init__.py
@@ -12,3 +12,13 @@ sys.path.append(os.path.abspath(os.path.join(os.path.abspath(sys.argv[0]), '..')
 sys.path.append(os.path.abspath(os.path.join(os.path.abspath(sys.argv[0]), '../..')))
 sys.path.append(os.path.abspath(os.path.join(os.path.abspath(sys.argv[0]), '../../..')))
 sys.path.append(os.path.abspath(os.path.join(os.path.abspath(sys.argv[0]), '../../../..')))
+
+logo = r"""
+    ____             __      __      _    __    _____             _        __              
+   / __ \  ____ _   / /_    / /_    (_)  / /_  / ___/    ____    (_)  ____/ /  ___    _____
+  / /_/ / / __ `/  / __ \  / __ \  / /  / __/  \__ \    / __ \  / /  / __  /  / _ \  / ___/
+ / _, _/ / /_/ /  / /_/ / / /_/ / / /  / /_   ___/ /   / /_/ / / /  / /_/ /  /  __/ / /    
+/_/ |_|  \__,_/  /_.___/ /_.___/ /_/   \__/  /____/   / .___/ /_/   \__,_/   \___/ /_/     
+                                                     /_/                                   
+"""
+sys.stdout.write(f'\033[0;35;1m{logo}\033[0m')
