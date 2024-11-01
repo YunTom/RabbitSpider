966f4c59394d764fabc685c466036baedc524042
YunTom <74497948+YunTom@users.noreply.github.com>
2024-11-01 16:31:13 +0800

    Update __init__.py

--- RabbitSpider/items/__init__.py
+++ RabbitSpider/items/__init__.py
@@ -1,13 +1,16 @@
-class Field(dict):
-    pass
+from typing import Dict
+from collections import defaultdict
 
 
 class ItemMeta(type):
     def __new__(mcs, name, bases, attrs):
-        field = {}
+        field: Dict[str, dict] = defaultdict(dict)
         for key, value in attrs.items():
-            if isinstance(value, Field):
-                field[key] = value
+            if not (callable(value) or key.startswith('_')):
+                field[key]['value'] = value
+            if key == '__annotations__':
+                for k, v in value.items():
+                    field[k]['annotation'] = v
         cls_instance = super().__new__(mcs, name, bases, attrs)
         cls_instance.FIELDS = field
         return cls_instance
