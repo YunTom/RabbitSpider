37f62fbedfac74de0c6f35d4f1f220da871b9959
yuntom <2395396520@qq.com>
2024-12-01 11:34:27 +0800

    1

--- RabbitSpider/spider/__init__.py
+++ RabbitSpider/spider/__init__.py
@@ -1,6 +1,7 @@
 from RabbitSpider import Request, Response
 from RabbitSpider.utils import event
 from RabbitSpider.utils.log import Logger
+from RabbitSpider.utils.control import SettingManager
 from curl_cffi.requests import AsyncSession
 
 
@@ -11,10 +12,11 @@ class Spider(object):
     def __init__(self, crawler):
         self.session: AsyncSession = crawler.session
         self.logger: Logger = crawler.logger
+        self.settings: SettingManager = crawler.settings
         crawler.subscriber.subscribe(self.spider_opened, event.spider_opened)
         crawler.subscriber.subscribe(self.spider_closed, event.spider_closed)
         crawler.subscriber.subscribe(self.spider_error, event.spider_error)
-        
+
     async def start_requests(self):
         """初始请求"""
         raise NotImplementedError
