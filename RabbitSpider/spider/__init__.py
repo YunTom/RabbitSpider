7b5b9e1fc156c49671107b6ad6b402800a91ff0b
YunTom <74497948+YunTom@users.noreply.github.com>
2024-12-16 14:13:36 +0800

    Update __init__.py

--- RabbitSpider/spider/__init__.py
+++ RabbitSpider/spider/__init__.py
@@ -1,4 +1,5 @@
-from typing import AsyncGenerator
+from asyncio import CancelledError
+from typing import AsyncGenerator, Union
 from RabbitSpider.utils.log import Logger
 from RabbitSpider import Request, Response, event, BaseItem
 from RabbitSpider.utils.control import SettingManager
@@ -20,11 +21,11 @@ class Spider(object):
         crawler.subscriber.subscribe(self.response_received, event.response_received)
         crawler.subscriber.subscribe(self.item_scraped, event.item_scraped)
 
-    async def start_requests(self) -> AsyncGenerator[Request] | None:
+    async def start_requests(self) -> AsyncGenerator[Request, None]:
         """初始请求"""
         raise NotImplementedError
 
-    async def parse(self, request: Request, response: Response) -> AsyncGenerator[Request | BaseItem] | None:
+    async def parse(self, request: Request, response: Response) -> AsyncGenerator[Union[Request, BaseItem, None], None]:
         """默认回调"""
         pass
 
@@ -36,18 +37,18 @@ class Spider(object):
         """爬虫关闭时触发"""
         pass
 
-    async def spider_error(self, error) -> None:
+    async def spider_error(self, error: Exception | CancelledError) -> None:
         """爬虫异常时触发"""
         pass
 
-    async def request_received(self, request) -> None:
+    async def request_received(self, request: Request) -> None:
         """发起请求时触发"""
         pass
 
-    async def response_received(self, response) -> None:
+    async def response_received(self, response: Response) -> None:
         """获取到响应时触发"""
         pass
 
-    async def item_scraped(self, item) -> None:
+    async def item_scraped(self, item: BaseItem) -> None:
         """生成item时触发"""
         pass
