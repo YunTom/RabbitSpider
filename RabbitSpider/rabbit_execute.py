33eb3782a507d82c8f139f880fe55d738e13cd35
YunTom <74497948+YunTom@users.noreply.github.com>
2025-03-17 11:48:05 +0800

    Update rabbit_execute.py

--- RabbitSpider/rabbit_execute.py
+++ RabbitSpider/rabbit_execute.py
@@ -8,22 +8,27 @@ from croniter import croniter
 from datetime import datetime
 from RabbitSpider.spider import Spider
 from RabbitSpider.core.engine import Engine
-from RabbitSpider.utils.control import TaskManager
+from RabbitSpider.utils.control import TaskManager, SettingManager
 from importlib.util import spec_from_file_location, module_from_spec
 
 
 async def go(spider_cls: Type[Spider], mode: str = 'auto', task_count: int = 1):
+    settings = SettingManager()
     for i in sys.argv[1:]:
         key, value = i.split('=')
         if key == 'mode':
             mode = value
         if key == 'task_count':
             task_count = int(value)
+    settings.set('MODE', mode)
+    settings.set('TASK_COUNT', task_count)
     async with Engine(mode, task_count) as engine:
         await engine.start(spider_cls())
 
 
 async def batch_go(spiders: List[Type[Spider]], task_pool: int = 10):
+    settings = SettingManager()
+    settings.set('MODE', 'auto')
     task_group: TaskManager = TaskManager(task_pool)
     async with Engine(mode='auto', task_count=task_pool) as engine:
         for spider_cls in spiders:
