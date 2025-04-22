c73f288b9326289c7f4ccbe460a648b115d7da1f
YunTom <74497948+YunTom@users.noreply.github.com>
2025-04-22 15:54:56 +0800

    Update allow_http_code.py

--- RabbitSpider/middlewares/allow_http_code.py
+++ RabbitSpider/middlewares/allow_http_code.py
@@ -7,6 +7,6 @@ class AllowHttpCodeMiddleware(BaseMiddleware):
         self.allow_http_code = settings.getlist('ALLOW_HTTP_CODES')
 
     async def process_response(self, request, response, spider):
-        if response.status not in self.allow_http_code:
-            self.logger.error(f'{request.to_dict()}，不允许的状态码：{response.status}',spider.name)
+        if response.status_code not in self.allow_http_code:
+            self.logger.error(f'{request.to_dict()}，不允许的状态码：{response.status_code}',spider.name)
             return True
