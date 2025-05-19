a754e84c514c5adaa702c292859b926d0f5abe4a
YunTom <74497948+YunTom@users.noreply.github.com>
2025-05-19 15:43:26 +0800

    Update download.py

--- RabbitSpider/core/download.py
+++ RabbitSpider/core/download.py
@@ -11,23 +11,23 @@ class CurlDownload(object):
     async def fetch(self, session, request) -> Response:
         if request['method'].upper() == 'GET':
             res = await session.get(request['url'],
-                                    params=request.get('params', None), cookies=request.get('cookies', None),
-                                    headers=request.get('headers', None), proxy=request.get('proxy', None),
+                                    params=request.get('params'), cookies=request.get('cookies'),
+                                    headers=request.get('headers'), proxy=request.get('proxy'),
                                     allow_redirects=request.get('allow_redirects', True),
                                     http_version=self.http_version,
                                     impersonate=self.impersonate,
-                                    timeout=request.get('timeout', 30)
+                                    timeout=request.get('timeout')
                                     )
 
         elif request['method'].upper() == 'POST':
             res = await session.post(request['url'],
-                                     data=request.get('data', None), json=request.get('json', None),
-                                     cookies=request.get('cookies', None), headers=request.get('headers', None),
-                                     proxy=request.get('proxy', None),
+                                     data=request.get('data'), json=request.get('json'),
+                                     cookies=request.get('cookies'), headers=request.get('headers'),
+                                     proxy=request.get('proxy'),
                                      http_version=self.http_version,
                                      impersonate=self.impersonate,
                                      allow_redirects=request.get('allow_redirects', True),
-                                     timeout=request.get('timeout', 60))
+                                     timeout=request.get('timeout'))
 
         else:
             raise RabbitExpect(f"{request['method']}请求方式未定义，请自定义添加！")
